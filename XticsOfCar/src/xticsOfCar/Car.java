450c8a35bdb2d99194cd91aecf45aa181aa0e789
lip2050 <lip2050@users.noreply.github.com>
2020-09-26 07:10:40 -0400

    This is the assignment from Week 3 Day 1

---
+++ XticsOfCar/src/xticsOfCar/Car.java
@@ -0,0 +1,14 @@
+package xticsOfCar;
+
+public class Car {
+	
+	public static int speed = 30;
+	public static String color = "Blue";
+	
+	public static void displaySpeed() {
+		System.out.println("The car has a beautiful color but has a poor speed of " + speed + " mil/hr");
+	}
+	public static void displayColor() {
+		System.out.println("The car has a " + color + "beautiful color ");
+	}
+}
---
+++ XticsOfCar/src/xticsOfCar/CarMain.java
@@ -0,0 +1,11 @@
+package xticsOfCar;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		
+		Car.displaySpeed();
+
+	}
+
+}
